dac96f0d7260988f8fdf40a7d7e97f767d936122
ssolson <ssolson@sandia.gov>
2023-04-27 21:28:33 -0600

    Make function

--- getPod.py
+++ getPod.py
@@ -1,11 +1,36 @@
 import getpodcast
 
-opt = getpodcast.options(
-    date_from='2023-04-20',
-    root_dir='./podcast')
+def get_podcast(url, name, start_at, path="./podcast"):
+    """
+    Download a podcast from a given url and save it to a given path.
+    
+    PARAMETERS
+    ----------
+    url : str
+        The url of the podcast to download.
+    name : str
+        The name of the podcast to download.
+    start_at : str  
+        The date from which to download the podcast.
+    path : str
+        The path to save the podcast to.
 
-podcasts = {
-    "thedailygwei": "https://thedailygwei.libsyn.com/rss"    
-}
+    RETURNS
+    -------
+    None
+    """
 
-getpodcast.getpodcast(podcasts, opt)
+    opt = getpodcast.options(
+        date_from=start_at,
+        root_dir=path)
+
+    podcasts = {name:  url}
+
+    getpodcast.getpodcast(podcasts, opt)
+
+
+get_podcast(
+    "https://thedailygwei.libsyn.com/rss" , 
+    "thedailygwei",
+    "2023-04-24",
+)
