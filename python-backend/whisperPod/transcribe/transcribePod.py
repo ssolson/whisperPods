ef27022475ad11330cf1716aac5eb18a75bcb891
ssolson <ssolson@sandia.gov>
2023-04-30 11:27:10 -0600

    only transcribe function

--- python-backend/whisperPod/transcribe/transcribePod.py
+++ python-backend/whisperPod/transcribe/transcribePod.py
@@ -1,79 +1,26 @@
-import os
-import re
-import fnmatch
 import whisper
-import pandas as pd
-# from pymongo.mongo_client import MongoClient
-from whisperPod.mongo import db
-
-# Replace 'your_database' with the name of your database and 'your_collection' with the name of your collection
-client = db.get_client()
-# db = client.your_database
-# collection = db.your_collection
 
 
-def find_mp3_files(path):
-    mp3_files = []
-
-    for root, dirnames, filenames in os.walk(path):
-        for filename in fnmatch.filter(filenames, "*.mp3"):
-            mp3_files.append(os.path.join(root, filename))
-
-    return mp3_files
-
-
-def whisper_podcast(filename):
-
-    model = whisper.load_model("tiny.en")
-    # model = whisper.load_model("medium.en")
+def whisper_podcast(filename, model="tiny.en"):
+    """
+    Transcribes an audio file using the Whisper library.
+    
+    Parameters
+    ----------
+    filename : str
+        The path to the audio file to transcribe.
+    model : str, optional
+        The name of the Whisper model to use for transcription. Defaults to "tiny.en".
+    
+    Returns
+    -------
+    str
+        The transcribed text of the audio file.
+    
+    Example
+    -------
+    >>> whisper_podcast("/path/to/audio/file.mp3")
+    'The transcribed text of the audio file.'
+    """
+    model = whisper.load_model(model)
     return model.transcribe(filename, fp16=False, language='English', verbose=True)
-
-if __name__ == "__main__":
-
-    folder_path = "podcast/thedailygwei/2023"
-    mp3_files = find_mp3_files(folder_path)
-
-    data = {
-        "Date": [],
-        "Episode Name": [],
-        "Podcast Name": [],
-        "Podcast Number": [],
-        "Subtitle": [],
-        "filename": [],
-    }
-
-    for file_path in mp3_files:
-        file = file_path.replace(f"{folder_path}\\", "")
-        # Extract components using regex
-        pattern = r"(\d{4}\.\d{2}\.\d{2}) (.+?) - (.+?) Refuel #(\d+) - (.+)\.mp3"
-        match = re.match(pattern, file)
-
-        if match:
-            date, episode_name, podcast_name, podcast_number, subtitle = match.groups()
-
-            # Append data to the dictionary
-            data["Date"].append(date)
-            data["Episode Name"].append(episode_name)
-            data["Podcast Name"].append(podcast_name)
-            data["Podcast Number"].append(int(podcast_number))
-            data["Subtitle"].append(subtitle)
-            data["filename"].append(file_path)
-
-        else:
-            print(f"Failed to extract components from the input string: {file}")
-
-    # Create a pandas DataFrame
-    df = pd.DataFrame(data)
-
-    # Convert the 'Date' column to datetime format and set it as the index
-    df['Date'] = pd.to_datetime(df['Date'], format='%Y.%m.%d')
-    df.set_index('Date', inplace=True)
-
-    print(df)
-
-    import ipdb; ipdb.set_trace()
-
-    transcript = whisper_podcast(df.iloc[0].filename)
-
-    import ipdb; ipdb.set_trace()
-    print(transcript['text'])
